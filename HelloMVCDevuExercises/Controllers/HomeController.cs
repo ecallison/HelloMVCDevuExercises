81ff5036ca603966747b2c4492f7f0c974871a73
ecallison <erinjccallison@yahoo.com>
2018-05-16 17:00:45 -0700

    Add project files.

---
+++ HelloMVCDevuExercises/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using HelloMVCDevuExercises.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloMVCDevuExercises.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var cars = HelloModel.GetCars();
+            return View(cars);
+        }
+    }
+}
